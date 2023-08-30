68d65b2f94c8c7b29dec9ff46fb65fe65b29c77a
chimo <chimo@chromic.org>
2023-08-30 18:19:17 -0400

    Pseudo-code

---
+++ private/config.dist.php
@@ -0,0 +1,13 @@
+<?php
+
+// Example config
+
+$config = [
+	'dbUser' => 'username',
+	'dbPassword' => 'password',
+	'dbHost' => 'localhost',
+	'dbName' => 'database_name',
+
+	'secret' => 'xxx'
+];
+
---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require '../private/config.php';
+
+if (!isset($_SERVER['HTTP_SECRET']) || $_SERVER['HTTP_SECRET'] !== $config['secret']) {
+	header($_SERVER['SERVER_PROTOCOL'] . ' 403 Forbidden');
+	exit(0);
+}
+
+$dbHost = $config['dbHost'] ?? 'localhost';
+
+$dsn = 'pgsql:host=' . $dbHost . ';dbname=' . $config['dbName'] . ';user=' .
+            $config['dbUser'] . ';password=' . $config['dbPassword'];
+
+$pdoConnection = new PDO($dsn);
+
+$coordinates = $pdoConnection->query(
+    'SELECT longitude,latitude '
+    . 'FROM positions '
+    . 'WHERE user_id = 1 '
+    . 'ORDER BY time '
+    . 'DESC LIMIT 1;'
+)[0];
+
+header('Content-Type: application/json');
+echo json_encode($coordinates);
+
